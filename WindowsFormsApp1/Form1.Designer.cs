d57c6d7b8e961f9afa8ade5b7de8bd0473e48e9d
Dmitry Chupakhin <chupahkin@gmail.com>
2023-12-19 01:55:31 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 78F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2F));
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93.33334F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666666F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1262, 568);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(6, 527);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(244, 38);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Редактор графа";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(259, 527);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(969, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Leonov PM LSTU";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(256, 3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(975, 521);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить узел";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(3, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 67);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Добавить ребро";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(3, 149);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(238, 67);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Добавить дугу";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.label5, 0, 6);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 5);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.button4, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.button3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(6, 6);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 7;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(244, 515);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(3, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(238, 67);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Удалить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 73);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Редактор графа";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(3, 365);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(238, 73);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Редактор графа";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 438);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(238, 77);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Редактор графа";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 568);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            draw_picture_box_image();
+        }
+
+        private bool isAddNewNodeClick = false;
+        private bool isAddNewArcClick = false;
+        private bool isAddNewArcActive = false;
+        private bool isAddNewEdgeClick = false;
+        private bool isAddNewEdgeActive = false;
+        private bool isRemoveButtonClick = false;
+
+        private Button activeButton = null;
+
+        private List<Arc> arcs = new List<Arc>();
+        private List<Edge> edges = new List<Edge>();
+        private List<Node> nodes = new List<Node>();
+
+        Node firstNode = null;
+        Node secondNode = null;
+
+        private Node currentNode;
+
+        class Arc
+        {
+            public Node Node1;
+            public Node Node2;
+
+            public void Draw(Graphics graphics)
+            {
+                if (Node1 == Node2)
+                {
+                    // Рисуйте петлю вокруг узла
+                    int x = Node1.Center.X - Node1.Radius;
+                    int y = Node1.Center.Y - Node1.Radius;
+                    int width = 2 * 40;
+                    int height = 2 * 40;
+
+                    graphics.DrawEllipse(Pens.Black, x, y, width, height);
+                    DrawArrow(graphics, Pens.Black, Node1.Center, Node2.Center);
+                }
+                else
+                {
+                    // Рисуйте линию между двумя узлами
+                    graphics.DrawLine(Pens.Black, Node1.Center, Node2.Center);
+
+                    DrawArrow(graphics, Pens.Black, Node1.Center, Node2.Center);
+                }
+
+            }
+            private void DrawArrow(Graphics g, Pen pen, Point p1, Point p2)
+            {
+                // Определите координаты для стрелки
+                float angle = (float)Math.Atan2(p2.Y - p1.Y, p2.X - p1.X);
+                float arrowLength = 40; // Длина стрелки
+
+                PointF arrowPoint1 = new PointF(p2.X - arrowLength * (float)Math.Cos(angle - Math.PI / 6),
+                                                p2.Y - arrowLength * (float)Math.Sin(angle - Math.PI / 6));
+                PointF arrowPoint2 = new PointF(p2.X - arrowLength * (float)Math.Cos(angle + Math.PI / 6),
+                                                p2.Y - arrowLength * (float)Math.Sin(angle + Math.PI / 6));
+
+                // Рисуйте линию и стрелку
+                g.DrawLine(pen, p1, p2);
+                g.FillPolygon(new SolidBrush(pen.Color), new PointF[] { p2, arrowPoint1, arrowPoint2 });
+            }
+        }
+        
+
+        class Edge
+        {
+            public Node Node1;
+            public Node Node2;
+
+            public void Draw(Graphics graphics)
+            {
+                if (Node1 == Node2)
+                {
+                    // Рисуйте петлю вокруг узла
+                    int x = Node1.Center.X - Node1.Radius;
+                    int y = Node1.Center.Y - Node1.Radius;
+                    int width = 2 * 40;
+                    int height = 2 * 40;
+
+                    graphics.DrawEllipse(Pens.Black, x, y, width, height);
+                }
+                else
+                {
+                    // Рисуйте линию между двумя узлами
+                    graphics.DrawLine(Pens.Black, Node1.Center, Node2.Center);
+                }
+
+            }
+        }
+
+        class Node
+        {
+            public int Number = 0;
+            public Point Center { get; set; }
+            public int Radius { get; set; }
+
+            public Node(Point center, int radius, int number)
+            {
+                Center = center;
+                Radius = radius;
+                Number = number;
+            }
+            public bool IsClickInRadius(Point clickPoint)
+            {
+                // Вычислите расстояние между точкой клика и центром
+                double distance = Math.Sqrt(Math.Pow(clickPoint.X - Center.X, 2) + Math.Pow(clickPoint.Y - Center.Y, 2));
+
+                // Проверьте, находится ли точка в радиусе
+                return distance <= Radius;
+            }
+            public void Draw(Graphics graphics)
+            {
+                Pen pen = new Pen(Color.Black);
+                Font font = new Font("Arial", 12);
+                Brush brush = Brushes.Black;
+
+                int diameter = Radius * 2;
+                int x = Center.X - Radius;
+                int y = Center.Y - Radius;
+
+                int ellipseX = Center.X - Radius;
+                int ellipseY = Center.Y - Radius;
+                int ellipseWidth = 2 * Radius;
+                int ellipseHeight = 2 * Radius;
+
+                graphics.FillEllipse(Brushes.White, ellipseX, ellipseY, ellipseWidth, ellipseHeight);
+
+                // Рисуем круг
+                graphics.DrawEllipse(pen, x, y, diameter, diameter);
+
+                // Создаем прямоугольник, ограничивающий круг
+                RectangleF rectangle = new RectangleF(x, y, diameter, diameter);
+
+                // Создаем объект форматирования строки
+                StringFormat stringFormat = new StringFormat();
+                stringFormat.Alignment = StringAlignment.Center;
+                stringFormat.LineAlignment = StringAlignment.Center;
+
+                // Рисуем текст внутри круга
+                graphics.DrawString(Number.ToString(), font, brush, rectangle, stringFormat);
+            }
+        }
+
+        private void draw_picture_box_image()
+        {
+            Bitmap emptyBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            // Используйте Graphics для рисования пустого изображения
+            using (Graphics g = Graphics.FromImage(emptyBitmap))
+            {
+                g.Clear(Color.White); // Установите цвет фона, если необходимо
+                Pen gridPen = new Pen(Color.Gray);
+
+                // Задайте размер ячейки сетки (например, 20x20 пикселей)
+                int cellSize = 20;
+
+                // Рисуйте горизонтальные линии сетки
+                for (int y = 0; y < pictureBox1.Height; y += cellSize)
+                {
+                    g.DrawLine(gridPen, 0, y, pictureBox1.Width, y);
+                }
+
+                // Рисуйте вертикальные линии сетки
+                for (int x = 0; x < pictureBox1.Width; x += cellSize)
+                {
+                    g.DrawLine(gridPen, x, 0, x, pictureBox1.Height);
+                }
+                foreach (var edge in edges)
+                {
+                    edge.Draw(g);
+                }
+                foreach (var arc in arcs)
+                {
+                    arc.Draw(g);
+                }
+                // Рисуйте узлы поверх сетки
+                foreach (var node in nodes)
+                {
+                    node.Draw(g);
+                }
+            }
+
+            label2.Text = "Узлов: " + nodes.Count;
+            label4.Text = "Ребра: " + edges.Count;
+            label5.Text = "Дуги: " + arcs.Count;
+            // Установите очищенное изображение в PictureBox
+            pictureBox1.Image = emptyBitmap;
+
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+
+            if (isAddNewNodeClick)
+            {
+                MouseEventArgs mouseArgs = e as MouseEventArgs;
+                Point clickPoint = mouseArgs.Location;
+
+                List<Node> sortedList = nodes.OrderBy(node => node.Number).ToList();
+
+                int current_number = 0;
+                foreach (var node in sortedList)
+                {
+                    if (node.Number != current_number)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        current_number += 1;
+                    }
+                }
+
+                // Создаем объект узла с координатами клика и произвольным радиусом
+                Node new_node = new Node(clickPoint, 20, current_number);
+
+                nodes.Add(new_node);
+
+                draw_picture_box_image();
+            }
+            else if (isRemoveButtonClick)
+            {
+                MouseEventArgs mouseArgs = e as MouseEventArgs;
+                Point clickPoint = mouseArgs.Location;
+
+                Node nodeToRemove = nodes.FirstOrDefault(node => node.IsClickInRadius(clickPoint));
+
+                if (nodeToRemove != null)
+                {
+                    // Если найден узел в радиусе клика, удалите его из коллекции
+                    nodes.Remove(nodeToRemove);
+                }
+
+                List<Edge> edgesToRemove = edges.Where(edge => edge.Node1 == nodeToRemove || edge.Node2 == nodeToRemove).ToList();
+                foreach (Edge edge in edgesToRemove)
+                {
+                    edges.Remove(edge);
+                }
+
+                List<Arc> arcsToRemove = arcs.Where(arc => arc.Node1 == nodeToRemove || arc.Node2 == nodeToRemove).ToList();
+                foreach (Arc arc in arcsToRemove)
+                {
+                     arcs.Remove(arc);
+                }
+
+                draw_picture_box_image();
+            }
+            else if (isAddNewEdgeClick)
+            {
+                MouseEventArgs mouseArgs = e as MouseEventArgs;
+                Point clickPoint = mouseArgs.Location;
+                if (isAddNewEdgeActive == false)
+                {
+                    firstNode = nodes.FirstOrDefault(node => node.IsClickInRadius(clickPoint));
+                    if (firstNode != null)
+                    {
+                        isAddNewEdgeActive = true;
+                    }
+                }
+                else
+                {
+                    secondNode = nodes.FirstOrDefault(node => node.IsClickInRadius(clickPoint));
+                    if (secondNode != null)
+                    {
+                        Edge edge = new Edge();
+                        edge.Node1 = firstNode;
+                        edge.Node2 = secondNode;
+                        edges.Add(edge);
+                        draw_picture_box_image();
+                        isAddNewEdgeActive = false;
+                    }
+                }
+            }
+            else if (isAddNewArcClick)
+            {
+                MouseEventArgs mouseArgs = e as MouseEventArgs;
+                Point clickPoint = mouseArgs.Location;
+                if (isAddNewArcActive == false)
+                {
+                    firstNode = nodes.FirstOrDefault(node => node.IsClickInRadius(clickPoint));
+                    if (firstNode != null)
+                    {
+                        isAddNewArcActive = true;
+                    }
+                }
+                else
+                {
+                    secondNode = nodes.FirstOrDefault(node => node.IsClickInRadius(clickPoint));
+                    if (secondNode != null)
+                    {
+                        Arc arc = new Arc();
+                        arc.Node1 = firstNode;
+                        arc.Node2 = secondNode;
+                        arcs.Add(arc);
+                        draw_picture_box_image();
+                        isAddNewArcActive = false;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (activeButton != null && (Button)sender != activeButton)
+            {
+                activeButton.PerformClick();
+            }
+
+            isAddNewNodeClick = !isAddNewNodeClick;
+            button1.BackColor = isAddNewNodeClick ? Color.Coral : SystemColors.Control;
+            activeButton = button1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (activeButton != null && (Button)sender != activeButton)
+            {
+                activeButton.PerformClick();
+            }
+            isRemoveButtonClick = !isRemoveButtonClick;
+            button4.BackColor = isRemoveButtonClick ? Color.Coral : SystemColors.Control;
+            activeButton = button4;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (activeButton != null && (Button)sender != activeButton)
+            {
+                activeButton.PerformClick();
+            }
+            isAddNewEdgeClick = !isAddNewEdgeClick;
+            button2.BackColor = isAddNewEdgeClick ? Color.Coral : SystemColors.Control;
+            activeButton = button2;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (activeButton != null && (Button)sender != activeButton)
+            {
+                activeButton.PerformClick();
+            }
+            isAddNewArcClick = !isAddNewArcClick;
+            button3.BackColor = isAddNewArcClick ? Color.Coral : SystemColors.Control;
+
+            activeButton = button3;
+        }
+    }
+}
